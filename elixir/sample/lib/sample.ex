b3914544606ff99f112e156c374f470b3712c8de
alejandroquesada <quesadaaba@gmail.com>
2019-07-04 01:35:26 -0400

    added elixir

--- elixir/sample/lib/sample.ex
+++ elixir/sample/lib/sample.ex
@@ -8,11 +8,28 @@ defmodule Sample do
 
   ## Examples
 
-      iex> Sample.hello()
-      :world
+      iex> Sample.addThree(4)
+      7
+
+      iex> Sample.prefixWithWarning("All is well.")
+      "[WARNING] All is well."
 
   """
-  def hello do
-    :world
+  # Example 1: Addition
+  def add a do
+    fn b -> a + b end
+  end
+
+  def addThree b do
+    Sample.add(3).(b)
+  end
+
+  # Example 2: String Prefixing
+  def addPrefix a do
+    fn b -> EEx.eval_string("[<%= a %>] <%= b %>", a: a, b: b) end
+  end
+
+  def prefixWithWarning a do
+    Sample.addPrefix("WARNING")
   end
 end
