47b138a1cfd3ae00939cd0303fc82ff7221b66a8
alejandroquesada <quesadaaba@gmail.com>
2019-07-04 01:21:46 -0400

    removed php. updated python.

--- php_7.x/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$string = "Hello, World!";
-echo $string
-
-?>
--- python_3.x/main.py
+++ python_3.x/main.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+# Example 1: Addition
+def add(a):
+    return lambda b: a + b
+addThree = add(3)
+print(addThree(4))
+
+# Example 2: String Prefixing
+# An alternative way of currying in Python
+addPrefix = lambda a: lambda b: f'[{a}] {b}'
+prefixWithWarning = addPrefix("WARNING")
+print(prefixWithWarning("All is well."))
+
+# Output:
+# $ python main.py
+# > 7
+# > [WARNING] All is well.
